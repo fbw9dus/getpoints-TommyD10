ccfd8b43993bdcd74fd467736951920f63c6eee6
Tomislav <tdumancic10@gmail.com>
2020-03-23 14:42:13 +0100

    it's working

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-<<<<<<< HEAD
 
 router.get('/', function(req, res, next) {
   var user = [{user:"fake", password:"fake"}]
@@ -9,10 +8,3 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
-=======
-router.get('/', function(req, res, next) {
-  res.json([{user: 'fake', password: 'fake'}]);
-});
-
-module.exports = router;
->>>>>>> part-2
