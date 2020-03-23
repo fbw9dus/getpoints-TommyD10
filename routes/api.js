12b6696c0b6efc9dc9b32fc610ac07fab3a4862d
Tomislav <tdumancic10@gmail.com>
2020-03-23 12:22:17 +0100

    finalFinal

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   var user = [{user:"fake", password:"fake"}]
-  res.status(200).json(user)
+  res.status(200).send(user)
 });
 
 module.exports = router;
