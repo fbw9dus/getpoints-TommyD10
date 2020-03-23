a9b3077c4ef181a5b9d8cfcb32b91d4ee2d9f1e7
Tomislav <tdumancic10@gmail.com>
2020-03-23 12:46:56 +0100

    first commit

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var apiRouter = require('./routes/api')
+var apiRouter = require('./routes/api');
+var patientsRouter = require('./routes/patients')
 
 var app = express();
 
@@ -18,5 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
+app.use('/patients',patientsRouter)
 
 module.exports = app;
